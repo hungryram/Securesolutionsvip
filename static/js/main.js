f3830a841e686041081a303a7b4ddb372e0b7423
Ram Dettmer <ramdettmer751@gmail.com>
2020-05-21 10:29:14 -0700

    updated site

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
-            
-    // Check for click events on the navbar burger icon
-        $(".navbar-burger").click(function() {
-        
-            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-            $(".navbar-burger").toggleClass("is-active");
-            $(".navbar-menu").toggleClass("is-active");
-        
-        });
-  
-        $('.sub-menu').click(function(){
-        $('.sub').toggleClass('open');
-         });
+
+// Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+    
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    
     });
-  
+
+    $('.sub-menu').click(function(){
+    $('.sub').toggleClass('sub-active-menu');
+     });
+});
